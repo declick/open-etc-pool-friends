af01d5162d88c65064b72b97494113aab1220d0a
declick <36468501+declick@users.noreply.github.com>
2025-08-12 17:15:36 +0200

    Delete docs/shutdown-pool.sh

--- docs/shutdown-pool.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Arrêt des services pool..."
-sudo systemctl stop api
-sudo systemctl stop nginx
-echo "Attente de 1 minute avant extinction..."
-sleep 60
-echo "Extinction de la machine..."
-sudo shutdown -h now
-
